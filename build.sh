2b41301855642f55ad73a6372a445276fa165696
yue.zhao <yue.zhao>
2022-07-05 21:51:35 +0800

    [Init] Add init doc file

---
+++ build.sh
@@ -0,0 +1,14 @@
+# Copyright (c) 2021 Horizon Robotics.All Rights Reserved.
+#
+# The material in this file is confidential and contains trade secrets
+# of Horizon Robotics Inc. This is proprietary information owned by
+# Horizon Robotics Inc. No part of this work may be disclosed,
+# reproduced, copied, transmitted, or used in any way for any purpose,
+# without the express written permission of Horizon Robotics Inc.
+
+set -ex
+cd $(dirname $0) || exit
+
+rm -rf build
+
+make html
